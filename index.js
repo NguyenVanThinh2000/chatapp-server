feb7cc0eb82abe81f259cfe8f2fcd3ecec1752c6
NguyenVanThinh2000 <18521448@gm.uit.edu.vn>
2022-12-23 17:25:26 +0700

    update code

--- index.js
+++ index.js
@@ -11,18 +11,26 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    socket.on("send-message", (data) => {
-        socket.emit("receive-message", data);
-    });
+  console.log(`${socket.id}: connected`);
+
+  socket.on("join-room", (request) => {
+    console.log(request);
+    socket.join(request.data.id);
+  });
+
+  socket.on("on-send", (request) => {
+    console.log(request);
+    io.to(request.data.roomId).emit("on-receive", request);
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`server is running on port: ${PORT}`);
+  console.log(`server is running on port: ${PORT}`);
 });
